30b1bb0b8bfb3619aa5915f1af4e3a5df9cb8030
JoNaulaerts <r0806158@student.thomasmore.be>
2022-02-10 19:24:13 +0100

    .

---
+++ 08-Arrays-01-Creation-Addition-Modification/Convert_Range_to_Array.rb
@@ -0,0 +1,16 @@
+letters_range = "A".."T"
+p letters_range
+
+
+letters_array = letters_range.to_a
+p letters_array.class
+p letters_array
+
+puts
+
+numbers_range = 450..500
+p numbers_range
+
+numbers_array = numbers_range.to_a
+p numbers_array.class
+p numbers_array
