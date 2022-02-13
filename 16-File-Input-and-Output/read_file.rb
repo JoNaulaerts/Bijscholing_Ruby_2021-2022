a7bff9337d06321c1b0c122a96778a12219ff7ed
JoNaulaerts <r0806158@student.thomasmore.be>
2022-02-13 10:29:46 +0100

    .

---
+++ 16-File-Input-and-Output/read_file.rb
@@ -0,0 +1,4 @@
+File.open("novel.txt", "r").each do |line|
+  puts line
+
+end
---
+++ 16-File-Input-and-Output/start.rb
@@ -0,0 +1,15 @@
+# puts "This is the beginning!"
+#
+# load "end.rb"
+#
+# puts "Alright, that was successful."
+#
+# load "./end.rb"
+#
+# if 8 > 5
+#   load "end.rb"
+# end
+
+puts "This is the beginning!"
+
+require "./end.rb"
